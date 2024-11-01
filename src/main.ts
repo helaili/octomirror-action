1910a95d5657e6462ba983b999afc05c9b02673b
Alain Hélaïli <alain@carbonhub.io>
2024-10-31 17:20:50 -0700

    Retrieve a pat and the server base url

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core'
 import * as httplib from '@actions/http-client'
 import { BearerCredentialHandler } from '@actions/http-client/lib/auth'
+import { Octokit } from "octokit";
 
 /**
  * The main function for the action.
@@ -8,25 +9,43 @@ import { BearerCredentialHandler } from '@actions/http-client/lib/auth'
  */
 export async function run(): Promise<void> {
   try {
-    const octomirrorAppUrl: string = core.getInput('octomirror-app-url')
+    const octomirrorAppUrl: string = core.getInput('app-url')
+    const serverUrl: string = core.getInput('server-url')
+    const pat: string = core.getInput('pat')
     const token: string = await core.getIDToken()
+    const initMode: boolean = core.getInput('init-mode') === 'true';
     const bearer: BearerCredentialHandler = new BearerCredentialHandler(token)
-    core.info(`token finishes with ${token.slice(-5)}`)
+    
     const http: httplib.HttpClient = new httplib.HttpClient('http-client', [
       bearer
     ])
-    const res: httplib.HttpClientResponse = await http.get(
-      `${octomirrorAppUrl}/api/listAllOrganizations`
-    )
 
-    if (res.message.statusCode !== 200) {
-      throw new Error(
-        `Failed to get organizations: ${res.message.statusMessage}`
+    const octokit = new Octokit({
+      auth: pat,
+      baseUrl: serverUrl,
+    })
+    
+    if(initMode) {
+      const res: httplib.HttpClientResponse = await http.get(
+        `${octomirrorAppUrl}/api/listAllOrganizations`
       )
+
+      if (res.message.statusCode !== 200) {
+        throw new Error(
+          `Failed to get organizations: ${res.message.statusMessage}`
+        )
+      }
+
+      const orgs = JSON.parse(await res.readBody())
+      core.setOutput('organizations', orgs)
+      createOrgs(octokit, orgs) 
     }
-    core.setOutput('organizations', JSON.parse(await res.readBody()))
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
+}
+
+async function createOrgs(octokit: Octokit, orgs: string[]) {
+  // Use octokit to create the orgs
 }
